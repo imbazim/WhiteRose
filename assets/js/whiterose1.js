edabe40f2b716f20791a664962c4293360c2833e
imbazim <basim.vt@live.com>
2023-11-03 15:56:24 +0530

    Initial release

---
+++ assets/js/whiterose1.js
@@ -0,0 +1,12 @@
+$('.bxslider').bxSlider({
+    auto: true,
+    autoControls: true,
+    stopAutoOnClick: true,
+    pager: true,
+    slideWidth: 600
+});
+
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true
+})
