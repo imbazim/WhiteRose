615b88581f686c3f6b20a5d93563e84ceb61f3c2
imbazim <basim.vt@live.com>
2023-12-14 10:45:37 +0530

    update 24

---
+++ assets/js/whiterose-2.js
@@ -0,0 +1,27 @@
+$('.bxslider').bxSlider({
+    auto: true,
+    autoControls: true,
+    stopAutoOnClick: true,
+    pager: true,
+    slideWidth: 600
+});
+
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true
+})
+
+$(document).on('ready', function() {
+    $(".modal").on("shown.bs.modal", function()  {
+        var urlReplace = "#" + $(this).attr('id');
+        history.pushState(null, null, urlReplace);
+    });
+
+    $(window).on('popstate', function() {
+        $(".modal").modal('hide');
+    });
+});
+
+$(".category_box").click(function() {
+   $("html, body").animate({ scrollTop: $('.product_row1').offset().top - 30 }, 'slow');
+});
